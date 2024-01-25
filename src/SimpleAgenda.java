d94cad5126c6a1a8d381de91c737f3475dd3c681
lab-109-inf-05 <enmanuelperez39@gmail.com>
2024-01-24 21:09:41 -0400

    Creacion de estructura de App

---
+++ src/SimpleAgenda.java
@@ -0,0 +1,18 @@
+public class SimpleAgenda {
+    public static void agregarContacto(String contacto){
+
+    }
+    public static void removerContacto(int id){
+
+    }
+    public static String actualizarContacto(int contacto){
+    return "N/A";
+    }
+    public static void  mostrarContactos(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
